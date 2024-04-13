8187ed680bf66c5f92654bb7aea200ba853d8ff7
Alex Perry <alex.perry422@gmail.com>
2024-04-13 16:15:15 -0400

    site header

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: 'Alex Perry - Dev',
+  url: 'https://example.com',
+  description: 'Nextjs 14 blog using velite, tailwind, and shadcn',
+  author: 'Alex Perry',
+  links: {
+    twitter: 'https://twitter.com/pezvstheworld',
+    github: 'https://github.com/alexp327',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
