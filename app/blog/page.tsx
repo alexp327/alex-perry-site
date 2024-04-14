c4555b6c0a876aa11ea2a6b64ad0c74d7706ec8f
Alex Perry <alex.perry422@gmail.com>
2024-04-14 18:12:33 -0400

    pagination

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,10 +2,25 @@ import React from 'react';
 import { posts } from '#site/content';
 import PostItem from '@/components/post-item';
 import { sortPosts } from '@/lib/utils';
+import QueryPagination from '@/components/query-pagination';
 
-const BlogListPage = async () => {
+const POSTS_PER_PAGE = 5;
+
+interface BlogPageProps {
+  searchParams: {
+    page?: string;
+  };
+}
+
+const BlogListPage = async ({ searchParams }: BlogPageProps) => {
+  const currentPage = Number(searchParams?.page || 1);
   const sortedPosts = sortPosts(posts.filter((post) => post.published));
-  const displayPosts = sortedPosts;
+  const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE);
+
+  const displayPosts = sortedPosts.slice(
+    POSTS_PER_PAGE * (currentPage - 1),
+    POSTS_PER_PAGE * currentPage
+  );
 
   return (
     <div className='container max-w-4xl py-6 lg:py-10'>
@@ -37,6 +52,7 @@ const BlogListPage = async () => {
       ) : (
         <p>Nothing to see here yet</p>
       )}
+      <QueryPagination totalPages={totalPages} className='justify-end mt-4' />
     </div>
   );
 };
