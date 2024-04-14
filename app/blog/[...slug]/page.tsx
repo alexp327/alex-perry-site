f5a340899a9bc34b0c8b63247ee22d7ec29f7298
Alex Perry <alex.perry422@gmail.com>
2024-04-14 17:46:31 -0400

    code block styling and mdx plugins

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -3,6 +3,8 @@ import { posts } from '#site/content';
 import { notFound } from 'next/navigation';
 import MDXContent from '@/components/mdx-components';
 
+import '@/styles/mdx.css';
+
 interface PostPageProps {
   params: {
     slug: string[];
