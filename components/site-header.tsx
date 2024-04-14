efb17b86501e13b1ad10c6e238aa7a33192ad935
Alex Perry <alex.perry422@gmail.com>
2024-04-14 12:32:54 -0400

    theme toggle

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -6,6 +6,7 @@ import { buttonVariants } from './ui/button';
 import { Icons } from './icons';
 import MainNav from './main-nav';
 import MobileNav from './mobile-nav';
+import ModeToggle from './mode-toggle';
 
 const siteHeader = () => {
   return (
@@ -44,6 +45,7 @@ const siteHeader = () => {
                 <span className='sr-only'>Twitter</span>
               </div>
             </Link>
+            <ModeToggle />
             <MobileNav />
           </nav>
         </div>
