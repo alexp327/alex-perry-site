218aa16b1feb8c786d32a4065a11978279d9bdbd
Alex Perry <alex.perry422@gmail.com>
2024-04-15 13:53:37 -0400

    add tags to post items

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -42,7 +42,7 @@ const BlogListPage = async ({ searchParams }: BlogPageProps) => {
       {displayPosts?.length > 0 ? (
         <ul className='flex flex-col'>
           {displayPosts.map((post) => {
-            const { slug, title, description, date } = post;
+            const { slug, title, description, date, tags } = post;
             return (
               <li key={slug}>
                 <PostItem
@@ -50,6 +50,7 @@ const BlogListPage = async ({ searchParams }: BlogPageProps) => {
                   title={title}
                   description={description}
                   date={date}
+                  tags={tags}
                 />
               </li>
             );
---
+++ components/tag.tsx
@@ -0,0 +1,27 @@
+import { slug } from 'github-slugger';
+import Link from 'next/link';
+import React from 'react';
+import { badgeVariants } from './ui/badge';
+
+interface TagProps {
+  tag: string;
+  current?: boolean;
+  count?: number;
+}
+
+const Tag = ({ tag, current, count }: TagProps) => {
+  return (
+    <Link
+      className={badgeVariants({
+        variant: current ? 'default' : 'secondary',
+        className: 'no-underline rounded-md',
+      })}
+      href={`/tags/${slug(tag)}`}
+    >
+      {tag}
+    </Link>
+  );
+};
+
+export default Tag;
+
