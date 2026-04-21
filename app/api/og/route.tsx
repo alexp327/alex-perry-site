15ac2703bddbd901dfd6ffbfe58696ac22600c31
Alex Perry <alex.perry422@gmail.com>
2026-04-21 02:29:58 +0000

    remove edge runtime from og image generation

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -3,10 +3,8 @@ import { ImageResponse } from 'next/og';
 import { siteConfig } from '@/config/site';
 import { Icons } from '@/components/icons';
 
-export const runtime = 'edge';
-
 const interBold = fetch(
-  new URL('../../../assets/fonts/Inter-Bold.ttf', import.meta.url)
+  new URL('../../../assets/fonts/Inter-Bold.ttf', import.meta.url),
 ).then((res) => res.arrayBuffer());
 
 export async function GET(req: NextRequest) {
@@ -24,26 +22,24 @@ export async function GET(req: NextRequest) {
       title.length > 140 ? `${title.substring(0, 140)}...` : title;
 
     return new ImageResponse(
-      (
-        <div tw='flex relative flex-col p-12 w-full h-full items-start text-black bg-white'>
-          <div tw='flex items-center'>
-            <Icons.logo tw='w-12 h-12' />
-            <p tw='ml-2 font-bold text-2xl'>{siteConfig.name}</p>
+      <div tw='flex relative flex-col p-12 w-full h-full items-start text-black bg-white'>
+        <div tw='flex items-center'>
+          <Icons.logo tw='w-12 h-12' />
+          <p tw='ml-2 font-bold text-2xl'>{siteConfig.name}</p>
+        </div>
+        <div tw='flex flex-col flex-1 py-10'>
+          <div tw='flex text-xl uppercase font-bold tracking-tight font-normal'>
+            BLOG POST
           </div>
-          <div tw='flex flex-col flex-1 py-10'>
-            <div tw='flex text-xl uppercase font-bold tracking-tight font-normal'>
-              BLOG POST
-            </div>
-            <div tw='flex text-[80px] font-bold text-[50px]'>{heading}</div>
-          </div>
-          <div tw='flex items-center w-full justify-between'>
-            <div tw='flex text-xl'>{siteConfig.url}</div>
-            <div tw='flex items-center text-xl'>
-              <div tw='flex ml-2'>{siteConfig.links.github}</div>
-            </div>
+          <div tw='flex text-[80px] font-bold text-[50px]'>{heading}</div>
+        </div>
+        <div tw='flex items-center w-full justify-between'>
+          <div tw='flex text-xl'>{siteConfig.url}</div>
+          <div tw='flex items-center text-xl'>
+            <div tw='flex ml-2'>{siteConfig.links.github}</div>
           </div>
         </div>
-      ),
+      </div>,
       {
         width: 1200,
         height: 630,
@@ -55,7 +51,7 @@ export async function GET(req: NextRequest) {
             weight: 700,
           },
         ],
-      }
+      },
     );
   } catch (error) {
     return new Response('Failed to generate image', { status: 500 });
